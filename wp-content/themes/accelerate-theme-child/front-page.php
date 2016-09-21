b9e1d297a755b48e6d3215b51039d6b67e2791cf
Alex Christoffel <alexchristoffel@yahoo.com>
2016-09-21 10:13:11 -0400

    functions php

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -11,8 +11,8 @@
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 1.0
  */
-
 get_header(); ?>
+
 <section class="home-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
@@ -24,17 +24,17 @@ get_header(); ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
 <section class="recent-posts">
-	<div class="site-content">
-	  <div class="blog-post">
-	    <h4>From the Blog</h4>
-	      <?php query_posts('posts_per_page=1'); ?>
-	        <?php while ( have_posts() ) : the_post(); ?>
-              <h2><?php the_title(); ?></h2>
-              <?php the_exceprt(); ?>
-              <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
-	        <?php endwhile; ?>
-	      <?php wp_reset_query(); ?>
-      </div>
-	</div>
+ <div class="site-content">
+  <div class="blog-post">
+   <h4>From the Blog</h4>
+    <?php query_posts('posts_per_page=1'); ?>
+     <?php while ( have_posts() ) : the_post(); ?>
+       <h2><?php the_title(); ?></h2>
+       <?php the_excerpt(); ?> 
+       <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+     <?php endwhile; ?> 
+    <?php wp_reset_query(); ?>
+   </div>
+  </div>
 </section>
 <?php get_footer(); ?>
